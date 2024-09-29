7b888a603b893f146491a565ecd5bc43f86f41ca
BalagwisJohnDenver <imbajhey@gmail.com>
2024-09-29 11:37:17 +0800

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
-        'rubik': ['Rubik', 'sans-serif'],
-        'londrina': ['Londrina Solid', 'cursive'],
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+        londrina: ["Londrina Solid", "cursive"],
       },
-      colors:{
-        White:'FEFFFE'
+      colors: {
+        White: "FEFFFE",
       },
-      backgroundImage:{
-        'hero-image': "url('./assets/images/image1.png')",
-        'Chicken-image': "url('./assets/images/chickenimg.png')",
-        'menu-cut': "url('./assets/images/hm-menu-cut.png')",
+      backgroundImage: {
+        "hero-image": "url('./assets/images/image1.png')",
+        "Chicken-image": "url('./assets/images/chickenimg.png')",
+        "menu-cut": "url('./assets/images/hm-menu-cut.png')",
       },
       rotate: {
-        '8': '8deg',
-      }
+        8: "8deg",
+      },
     },
   },
   plugins: [],
-}
-
+};
